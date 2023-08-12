6784c9687a725ff2dcd2724fcd89cddfd5c53948
Fanis Timerbulatov <TimerbulatovFT@gmail.com>
2023-08-12 22:16:09 +0500

    I create array inverter.

---
+++ src/com/company/lesson7/ArrayInverter.java
@@ -0,0 +1,16 @@
+package com.company.lesson7;
+
+import java.util.Arrays;
+
+public class ArrayInverter {
+    public static void main(String[] args) {
+        int[] array = {0,1,2,3,4,5,6,7};
+        System.out.println(Arrays.toString(array));
+        for (int i = 0; i<(array.length/2); i++){
+            int tmp = array[i];
+            array[i]=array[array.length-i-1];
+            array[array.length-i-1]=tmp;
+        }
+        System.out.println(Arrays.toString(array));
+    }
+}
